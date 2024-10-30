0b2052f35307c58fc9cc5fe5f0c7dd273cdbd77c
Charlie-BrownGT <108026345+Charlie-BrownGT@users.noreply.github.com>
2024-10-30 13:59:12 +0000

    Add files via upload

---
+++ build/CMakeFiles/sim.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/18NeAp21NaBuild/sim.cc" "CMakeFiles/sim.dir/sim.cc.o" "gcc" "CMakeFiles/sim.dir/sim.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/18NeAp21NaBuild/src/Action.cc" "CMakeFiles/sim.dir/src/Action.cc.o" "gcc" "CMakeFiles/sim.dir/src/Action.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/18NeAp21NaBuild/src/DetectorConstruction.cc" "CMakeFiles/sim.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/sim.dir/src/DetectorConstruction.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/18NeAp21NaBuild/src/Generator.cc" "CMakeFiles/sim.dir/src/Generator.cc.o" "gcc" "CMakeFiles/sim.dir/src/Generator.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/18NeAp21NaBuild/src/PhysicsList.cc" "CMakeFiles/sim.dir/src/PhysicsList.cc.o" "gcc" "CMakeFiles/sim.dir/src/PhysicsList.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
