a4d8f82f0e44e344d65bda92c617d80414cf92b0
Guy.Carter-Richardson <Guy.Carter-Richardson@outlook.com>
2024-11-04 21:28:26 +0000

    initial build

---
+++ build/CMakeFiles/sim.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/guy/software/geant4/projects/18NeAp21NaBuild-dev/sim.cc" "CMakeFiles/sim.dir/sim.cc.o" "gcc" "CMakeFiles/sim.dir/sim.cc.o.d"
+  "/home/guy/software/geant4/projects/18NeAp21NaBuild-dev/src/ActionInitialization.cc" "CMakeFiles/sim.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/sim.dir/src/ActionInitialization.cc.o.d"
+  "/home/guy/software/geant4/projects/18NeAp21NaBuild-dev/src/DetectorConstruction.cc" "CMakeFiles/sim.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/sim.dir/src/DetectorConstruction.cc.o.d"
+  "/home/guy/software/geant4/projects/18NeAp21NaBuild-dev/src/EventAction.cc" "CMakeFiles/sim.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/sim.dir/src/EventAction.cc.o.d"
+  "/home/guy/software/geant4/projects/18NeAp21NaBuild-dev/src/PhysicsList.cc" "CMakeFiles/sim.dir/src/PhysicsList.cc.o" "gcc" "CMakeFiles/sim.dir/src/PhysicsList.cc.o.d"
+  "/home/guy/software/geant4/projects/18NeAp21NaBuild-dev/src/PrimaryGeneratorAction.cc" "CMakeFiles/sim.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/sim.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/home/guy/software/geant4/projects/18NeAp21NaBuild-dev/src/RunAction.cc" "CMakeFiles/sim.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/sim.dir/src/RunAction.cc.o.d"
+  "/home/guy/software/geant4/projects/18NeAp21NaBuild-dev/src/StackingAction.cc" "CMakeFiles/sim.dir/src/StackingAction.cc.o" "gcc" "CMakeFiles/sim.dir/src/StackingAction.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
