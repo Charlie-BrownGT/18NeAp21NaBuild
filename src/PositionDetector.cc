d2ee8ac7216ac040a920cb194eda560c6622cad4
Guy.Carter-Richardson <Guy.Carter-Richardson@outlook.com>
2024-11-06 20:00:11 +0000

    get energy function working

--- src/PositionDetector.cc
+++ src/PositionDetector.cc
@@ -14,7 +14,11 @@ G4bool PositionDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhist)
 	G4StepPoint *preStepPoint = aStep->GetPreStepPoint();
 	
 	G4ThreeVector posIon = preStepPoint->GetPosition();
-	G4cout << "Ion position: " << posIon << G4endl;
-	
+	//G4cout << "Ion position: " << posIon << G4endl;
+
+	G4double kineticEnergy = track->GetKineticEnergy();
+	G4double totalEnergy = track->GetTotalEnergy();
+	G4cout << "Ion totalEnergy: " << totalEnergy << G4endl;
+
 	return true;
 }
